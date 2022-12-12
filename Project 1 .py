a296c8867f9cfa2d4c685b35ef9ebf6e775a3310
Nareshvelayuthan <120123472+Nareshvelayuthan@users.noreply.github.com>
2022-12-12 20:33:27 +0530

    Project 1 .py
    My captain project

---
+++ Project 1 .py
@@ -0,0 +1,5 @@
+#radius of the circle
+radius = float(input("Enter the radius:"))
+#area of circle for entered radius
+area = 3.14*(radius**2)
+print("The area of the circle with radius",radius," is",area)
