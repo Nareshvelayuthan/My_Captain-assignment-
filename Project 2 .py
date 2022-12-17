c5abe7312652a48dba3917ed8c0291e011307995
Nareshvelayuthan <120123472+Nareshvelayuthan@users.noreply.github.com>
2022-12-17 11:17:27 +0530

    Project 2.py

--- Project 2 .py
+++ Project 2 .py
@@ -1,10 +1,10 @@
-#To print fibanacci series
-A= int(input("Number of fibonacci series need to be displayed:"))
+#To print fibanacci series.
+a= int(input("Number of fibonacci series need to be displayed:"))
 i=0
 num1=0
 num2=1
 print(0,1,end=',')
-for i in range(A-2):
+for i in range(a-2):
  i=num1+num2
  print(i,end=',')
  num1=num2
