ee41c495303067bd8e50bddb933fd1ced062d7f1
Nareshvelayuthan <120123472+Nareshvelayuthan@users.noreply.github.com>
2022-12-17 11:11:43 +0530

    Project 2 .py

---
+++ Project 2 .py
@@ -0,0 +1,10 @@
+A= int(input("Number of fibonacci series need to be displayed:"))
+i=0
+num1=0
+num2=1
+print(0,1,end=',')
+for i in range(A-2):
+ i=num1+num2
+ print(i,end=',')
+ num1=num2
+ num2=i
