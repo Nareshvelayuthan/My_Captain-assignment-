790cdee112d9d7089eea507aa6cc29376e72b92f
Nareshvelayuthan <120123472+Nareshvelayuthan@users.noreply.github.com>
2022-12-17 12:15:17 +0530

    Project 3.py

---
+++ Project 3.py
@@ -0,0 +1,10 @@
+#To print all positive no's in a range
+list1=[12,-7,5,64,-14]
+for num in list1 :
+	if num>0:
+		print(num)
+        
+list2=[12,14,-95,3]
+for num in list2 :
+	    if num>0:
+	    	print(num)
