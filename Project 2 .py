a256d686cd9bbdf1ab4d1c96693a673e4c443220
Nareshvelayuthan <120123472+Nareshvelayuthan@users.noreply.github.com>
2022-12-24 19:59:45 +0530

    Project2 .py

--- Project 2 .py
+++ Project 2 .py
@@ -4,8 +4,8 @@ i=0
 num1=0
 num2=1
 print(0,1,end=',')
-for i in range(a-2):
- i=num1+num2
- print(i,end=',')
- num1=num2
- num2=i
+for i in range (a-2):
+    i=num1+num2
+    print(i,end=',')
+    num1=num2
+    num2=i
